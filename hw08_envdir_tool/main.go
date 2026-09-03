4e9f54a751e1a47a4b98513f2cfc18745acb1c90
ngorkunov <ngorkunov@diasoft.ru>
2026-09-03 13:57:42 +0300

    HW8 is completed

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "usage: go-envdir /path/to/env/dir command [args...]")
+		os.Exit(1)
+	}
+
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	code := RunCmd(os.Args[2:], env)
+	os.Exit(code)
 }
