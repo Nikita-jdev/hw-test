cafa25b2312c2fd41c246847c08857aa0c22c495
ngorkunov <ngorkunov@diasoft.ru>
2026-09-21 22:11:54 +0300

    HW14 is completed

--- hw12_13_14_15_16_calendar/internal/broker/kafka/kafka.go
+++ hw12_13_14_15_16_calendar/internal/broker/kafka/kafka.go
@@ -6,9 +6,8 @@ import (
 	"fmt"
 	"time"
 
-	kafkago "github.com/segmentio/kafka-go"
-
 	"github.com/fixme_my_friend/hw12_13_14_15_16_calendar/internal/broker"
+	kafkago "github.com/segmentio/kafka-go"
 )
 
 const (
